436bf8d77e94c37a51f0ee8901946fb61dc9c39a
k13nNg <nguyenhuutrungkien2105@gmail.com>
2022-12-29 16:16:15 -0500

    Set up teh project

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def add_tubes(request):
+    if (request.method == "POST"):
+        print()
+        return redirect('../index')
+
+    return render(request, 'add-tubes.html')
