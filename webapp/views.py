a20a1939cc7d5991ee81e639b84730e9573eb38d
k13nNg <nguyenhuutrungkien2105@gmail.com>
2022-12-31 22:59:39 -0500

    Store tubes in 2D arrays

--- webapp/views.py
+++ webapp/views.py
@@ -1,14 +1,26 @@
 
 from django.shortcuts import render, redirect
 
+# temp_arr = []
+tubes_collection = []
+tube = []
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'tubes_collection': tubes_collection})
 
 def add_tubes(request):
+    global tubes_collection, tube
+
     if (request.method == "POST"):
-        print()
+        for i in range (1, int(request.POST.get("ball-num", None))+1):
+            # print(request.POST.get("ball-"+str(i), None))
+            tube.append(request.POST.get("ball-"+str(i), None))
+        
+        tubes_collection.append(tube)
+        tube = []
+
         return redirect('../index')
 
     return render(request, 'add-tubes.html')
